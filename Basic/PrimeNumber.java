271fe0f4c8795d4a6570f344f2455404ebd24914
Sankha Subhra Mondal <sankha.github@gmail.com>
2022-02-20 23:43:23 +0530

    Prime number

---
+++ Basic/PrimeNumber.java
@@ -0,0 +1,25 @@
+/*  Write a program to print a number is Prime or Not  */
+   
+import java.util.Scanner;
+
+class PrimeNumber {    
+     public static void main(String args[]) {
+     Scanner sc = new Scanner(System.in);
+     int flag=0;      
+     int n = sc.nextInt();  
+
+  if(n==0 || n==1) 
+        System.out.println(n+" is not prime number");      
+  else {  
+        for(int i=2;i<=n/2;i++){      
+        if(n%i==0){      
+            System.out.println(n+" is not prime number");      
+            flag=1;      
+            break;      
+        }      
+   }      
+   if(flag==0)   
+        System.out.println(n+" is prime number"); }  
+   
+    }    
+}
