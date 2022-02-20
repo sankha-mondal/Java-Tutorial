5180fee32378855e09692029f2ed9c75ad5441ae
Sankha Subhra Mondal <sankha.github@gmail.com>
2022-02-20 23:46:16 +0530

    Summation of two numbers

---
+++ Basic/Summation.java
@@ -0,0 +1,10 @@
+/*  Summation of two numbers using java  */
+
+class Summation {
+	public static void main(String[] args) {
+		int x = 50;
+		int y = 20;
+		int z = x+y;
+		System.out.println(z);
+	}
+}
