1789f6f144b12623e63a5d52b99e3f302b3b24d7
Sankha Subhra Mondal <sankha.github@gmail.com>
2022-02-20 23:44:41 +0530

    Subtraction of two numbers

---
+++ Basic/Subtraction.java
@@ -0,0 +1,10 @@
+/*  Subtraction of two numbers using java  */
+
+class Subtraction {
+	public static void main(String[] args) {
+		int x = 50;
+		int y = 20;
+		int z = x-y;
+		System.out.println(z);
+	}
+}
