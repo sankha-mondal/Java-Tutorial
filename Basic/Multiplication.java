904715d9d50b2145a6a39d40948d28fa71205319
Sankha Subhra Mondal <sankha.github@gmail.com>
2022-02-20 23:41:32 +0530

    Multiplication of two numbers

---
+++ Basic/Multiplication.java
@@ -0,0 +1,10 @@
+/* Multiplication of two numbers using java  */
+
+class Multiplication {
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 2;
+		int z = x*y;
+		System.out.println(z);
+	}
+}
