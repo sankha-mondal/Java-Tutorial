0d66c21a51e48b16cfc7cfbf983631373e54ae0c
Sankha Subhra Mondal <sankha.github@gmail.com>
2022-02-20 23:34:38 +0530

    Dividation.java

---
+++ Basic/Dividation.java
@@ -0,0 +1,10 @@
+/* Dividation of two numbers using java  */
+
+class Dividation {
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 2;
+		int z = x/y;
+		System.out.println(z);
+	}
+}
