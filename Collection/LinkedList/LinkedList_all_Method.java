564367704e95710523466cc937e864123bdfba19
Sankha Subhra Mondal <sankha.github@gmail.com>
2022-02-28 23:41:26 +0530

    LinkedList all Methods in one

--- Collection/LinkedList/LinkedList_all_Method.java
+++ Collection/LinkedList/LinkedList_all_Method.java
@@ -23,77 +23,77 @@ public class LinkedList_all_Method {
 					
 System.out.println("\n------------------------------------------------------------------");	
 
-			System.out.println("LinkedList addFirst() method: To add list element at 0th index");
-			car.addFirst("Maruti");
-		    System.out.println(car);
+		System.out.println("LinkedList addFirst() method: To add list element at 0th index");
+		car.addFirst("Maruti");
+	    	System.out.println(car);
 
 System.out.println("\n------------------------------------------------------------------");
 
-			System.out.println("LinkedList addLast() method: To add list element at Last-th index");
-			car.addLast("Maruti");
-			System.out.println(car);
+		System.out.println("LinkedList addLast() method: To add list element at Last-th index");
+		car.addLast("Maruti");
+		System.out.println(car);
 
 System.out.println("------------------------------------------------------------------"); 
 			
-			System.out.println("\nArrayList set() method: To Change an Item");
-			car.set(2,"Royal");
-			System.out.println(car);
+		System.out.println("\nArrayList set() method: To Change an Item");
+		car.set(2,"Royal");
+		System.out.println(car);
 			
 System.out.println("------------------------------------------------------------------"); 
 			
-			System.out.println("\nLinkedList get() method: To Access an Item");
-			System.out.println(car.get(2));
+		System.out.println("\nLinkedList get() method: To Access an Item");
+		System.out.println(car.get(2));
 			
 System.out.println("------------------------------------------------------------------"); 	
 
-			System.out.println("\nArrayList remove() method: To remove particular element from LinkedList");
-			car.remove(0);	
-			System.out.println(car);
+		System.out.println("\nArrayList remove() method: To remove particular element from LinkedList");
+		car.remove(0);	
+		System.out.println(car);
 			
 System.out.println("\n------------------------------------------------------------------");
 
-			System.out.println("LinkedList removeFirst() method: To remove list element at 0th index");
-			car.removeFirst();
-		    System.out.println(car);
+		System.out.println("LinkedList removeFirst() method: To remove list element at 0th index");
+		car.removeFirst();
+                                      System.out.println(car);
 		    
 System.out.println("\n------------------------------------------------------------------");		    
 			
-			System.out.println("LinkedList removeLast() method: To remove list element at last-th index");
-			car.removeLast();
-		    System.out.println(car);
+		System.out.println("LinkedList removeLast() method: To remove list element at last-th index");
+		car.removeLast();
+		 System.out.println(car);
 		    
 System.out.println("------------------------------------------------------------------");
 
-			System.out.println("LinkedList getFirst() method: To fatch 1st element of List");
-			System.out.println(car.getFirst());
+		System.out.println("LinkedList getFirst() method: To fatch 1st element of List");
+		System.out.println(car.getFirst());
 			
 System.out.println("------------------------------------------------------------------");			
 			
-			System.out.println("LinkedList getLast() method: To fatch Last element of List");
-			System.out.println(car.getLast());
+		System.out.println("LinkedList getLast() method: To fatch Last element of List");
+		System.out.println(car.getLast());
 			
 System.out.println("\n------------------------------------------------------------------");	
 
-			System.out.println("LinkedList contains() method: To detect present or not");			
-			boolean ans = car.contains("BMW");
-			System.out.println(ans);
+		System.out.println("LinkedList contains() method: To detect present or not");			
+		boolean ans = car.contains("BMW");
+		System.out.println(ans);
 			
 System.out.println("\n------------------------------------------------------------------");	
 
-			System.out.println("ArrayList indexOf() method: To identify index");
-			int pos = car.indexOf("Royal");
-			System.out.println("The element is at index : " + pos);
+		System.out.println("ArrayList indexOf() method: To identify index");
+		int pos = car.indexOf("Royal");
+		System.out.println("The element is at index : " + pos);
 			
 System.out.println("------------------------------------------------------------------"); 	
 
-			System.out.println("\nLoop Through an ArrayList");
-			System.out.println("ArrayList Elements: ");
-			for (int i=0; i<car.size(); i++) {
-				System.out.print(car.get(i) + "  ");
+		System.out.println("\nLoop Through an ArrayList");
+		System.out.println("ArrayList Elements: ");
+		       for (int i=0; i<car.size(); i++) {
+			System.out.print(car.get(i) + "  ");
 			}			
 			
 			
 			
 			
-		  }
+	}
 }
