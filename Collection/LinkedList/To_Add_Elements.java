4773a0fb88e2b31ab753a1758d50e2129ecfa207
Sankha Subhra Mondal <sankha.github@gmail.com>
2022-02-28 12:54:40 +0530

    To add elements in LinkedList

---
+++ Collection/LinkedList/To_Add_Elements.java
@@ -0,0 +1,17 @@
+package com.ArrayList;
+
+import java.util.*;
+
+
+public class To_Add_Elements {
+	public static void main(String[] args) { 
+    	LinkedList<String> cars = new LinkedList<String>();
+   
+	 cars.add("Volvo");
+   	 cars.add("BMW");
+    	 cars.add("Ford");
+    	 cars.add("Mazda");
+   	 System.out.println(cars);
+  	}
+
+}
