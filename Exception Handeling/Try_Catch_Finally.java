106f2fa7b19867340c771d522eaf6b23ed0918ea
Sankha Subhra Mondal <sankha.github@gmail.com>
2022-03-01 21:02:22 +0530

    Try Catch Finally Syntax

---
+++ Exception Handeling/Try_Catch_Finally.java
@@ -0,0 +1,19 @@
+package com.try_catch;
+
+public class Try_Catch_Finally {
+	public static void main(String[] args) {
+
+		try {
+			int res = 10/0;
+			System.out.println(res);
+		}
+		catch (ArithmeticException e) {
+			System.out.println("Divide by zero not possible: " + e.getMessage());  // Normal
+			throw new ArithmeticException("Divide by zero not possible");    // Custom Exception
+		}
+		finally {
+			System.out.println("Code is terminated successfully.");
+		}
+	}
+
+}
