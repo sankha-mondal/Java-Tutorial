b6cd5bd36151c2fc121570e4107347f11e9042d7
Sankha Subhra Mondal <sankha.github@gmail.com>
2022-03-03 01:17:21 +0530

    Stack all Methods in one

--- Collection/List/Vector/Stack/Stack_all_Methods.java
+++ Collection/List/Vector/Stack/Stack_all_Methods.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class Stack_all_Methods {
 	public static void main(String[] args) {
 		
-		Stack<Integer> stk= new Stack<>();     								//  Declaration of stack
+		Stack<Integer> stk= new Stack<>();     							//  Declaration of stack
 		
 		System.out.println("Stack empty():To ckeck stack is empty or not");
 		boolean result = stk.empty();  
@@ -15,7 +15,7 @@ System.out.println("\n----------------------------------------------------------
 
 		System.out.println("Stack push():To store into stack");
 		stk.push(10);  
-		stk.push(20);  													//  push()
+		stk.push(20);  											//  push()
 		stk.push(30);  					
 		stk.push(40);  
 		System.out.println("Elements in Stack: " + stk);  
@@ -23,20 +23,20 @@ System.out.println("\n----------------------------------------------------------
 System.out.println("\n------------------------------------------------------------------");
 
 		System.out.println("Stack peek():To print last entry but not delete from stack");
-		int last = stk.peek();													//  peek()
+		int last = stk.peek();										//  peek()
 		System.out.println("Last entry element in Stack: " + last); 
 
 System.out.println("\n------------------------------------------------------------------");
 
 		System.out.println("Stack pop():To print last entry and remove from stack");
-		int remove = stk.pop();												//  pop()
+		int remove = stk.pop();										//  pop()
 		System.out.println("POP element of Stack: " + remove);
 		System.out.println("Elements in Stack: " + stk);
 
 System.out.println("\n------------------------------------------------------------------");
 
 		System.out.println("Stack search():To  To identify index in stack");
-		int found = stk.search(20);												//  search()
+		int found = stk.search(20);									//  search()
 		System.out.println("Element found at index: " + found);
 	}
 }
