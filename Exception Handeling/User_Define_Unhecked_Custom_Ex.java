ff68602ed5f2dee7dcb3f530a223591d80a0d5dc
Sankha Subhra Mondal <sankha.github@gmail.com>
2022-03-06 20:16:13 +0530

    User define Un-checked Custom exception

--- Exception Handeling/User_Define_Unhecked_Custom_Ex.java
+++ Exception Handeling/User_Define_Unhecked_Custom_Ex.java
@@ -14,7 +14,7 @@ public class User_Define_Unchecked_Ex {
 	public static void main(String[] args) {
 		
 		int age = 16;
-		if(age<18) {
+		if(age < 18) {
 			throw new UnderAgeException("Not elligible for vote");
 		}
 		else {
