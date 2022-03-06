04a3546c73ee7359c0b574e150c36038291f751e
Sankha Subhra Mondal <sankha.github@gmail.com>
2022-03-06 20:02:10 +0530

    Thread Behaviour

---
+++ Thread/Thread_Behaviour.java
@@ -0,0 +1,39 @@
+
+
+
+class Thread1 extends Thread {
+	public void run() {
+		try {
+			System.out.println("Thread 1 is running");
+		}
+		catch (Exception e) {
+			System.out.println("Something went wrong: " + e);
+		}
+	}
+}
+
+
+class Thread2 implements Runnable {
+	public void run() {
+		try {
+			System.out.println("Thread 2 is running");
+		}
+		catch (Exception e) {
+			System.out.println("Something went wrong: " + e);
+		}
+	}
+}
+
+
+public class Thread_Behaviour {
+	public static void main(String[] args) {
+
+		for(int i=0;i<5;i++) {
+			Thread1 t1 = new Thread1();
+			t1.start();
+			
+			Thread t2 = new Thread(new Thread2());
+			t2.start();
+		}
+	}
+}
