986e7965914dac19210772588c2dfa3c2e4fc72d
Sankha Subhra Mondal <sankha123@gmail.com>
2022-03-13 01:55:10 +0530

    Naming Thread

---
+++ Thread/Naming_Thread.java
@@ -0,0 +1,24 @@
+	//  	Naming of Thread - Thread.currentThread().getName()
+
+
+public class Naming_Thread extends Thread { 
+		
+		public static void main(String[] args){  
+				Demon_Thread t1 = new Demon_Thread();		//  creating thread  
+				Demon_Thread t2 = new Demon_Thread();  		//  creating thread 
+				Demon_Thread t3 = new Demon_Thread();  		//  creating thread 
+			  
+				Thread.currentThread().setName("Thread1");
+				System.out.println(Thread.currentThread().getName());
+				System.out.println(t1.currentThread().getName());
+				System.out.println(t2.currentThread().getName());
+				
+				Thread.currentThread().setName("Thread2");
+				System.out.println(Thread.currentThread().getName());
+				System.out.println(t1.currentThread().getName());
+				System.out.println(t2.currentThread().getName());
+			
+			  
+			
+	}
+}
