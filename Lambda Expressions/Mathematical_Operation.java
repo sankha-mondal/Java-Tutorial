3bd5f0deed276f788ff2d43a64d8484c3d04339f
Sankha Subhra Mondal <sankha123@gmail.com>
2022-03-14 00:39:33 +0530

    Mathematical Operation

---
+++ Lambda Expressions/Mathematical_Operation.java
@@ -0,0 +1,25 @@
+
+interface Math {
+	int getSqure(int n);
+}
+
+public class Mathematical_Operation {
+	public static void main(String[] args) {
+
+		System.out.println("Anonymously: ");
+		Math obj1 = new Math() {
+			
+			@Override						//  Anonymously
+			public int getSqure(int n) {
+				return n*n;
+			}
+		};
+		System.out.println("The Squre of 5: "+ obj1.getSqure(5));
+		
+		
+		System.out.println("\nUsing Lambda Expression: ");
+		Math obj2 = (n) -> n*n;									//   Lambda Expression
+		System.out.println("The Squre of 5: "+ obj2.getSqure(5));
+		
+	}
+}
