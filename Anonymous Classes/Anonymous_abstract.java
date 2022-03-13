f6954a65ed94347c90eff4a5752c36492f9cf8f1
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2022-03-13 22:23:31 +0530

    Anonymous Class -> [abstract]

--- Anonymous Classes/Anonymous_abstract.java
+++ Anonymous Classes/Anonymous_abstract.java
@@ -1,4 +1,7 @@
- 
+	//   Anonymous class is nothing but a class without any name.
+	//   They are used to override a class method or interface.
+	//   Anonymous classes in Java help us to write more concise and readable code.
+
 abstract class Vehicle{
     abstract void drive();
 }
