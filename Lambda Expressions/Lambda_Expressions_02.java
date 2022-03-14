22460eb205520c843592c69396e8b46b5d23f91c
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2022-03-14 21:17:52 +0530

    Lambda Expressions Example -> 02

--- Lambda Expressions/Lambda_Expressions_02.java
+++ Lambda Expressions/Lambda_Expressions_02.java
@@ -1,9 +1,12 @@
 //  Lambda expressions were introduced in Java 8.
 //  They are similar to methods, but they don't need a name.
-//  interface is must
+//  Interface is must be  Functional Interface, default, Static method 
 
 interface LambaExpt {
 	void m1(String s, int r);
+	default void m2() {
+		System.out.println("Hello Eneryone");
+	}
 }
 
 public class Lambda_Expressions_02 {
@@ -13,6 +16,7 @@ public class Lambda_Expressions_02 {
 			System.out.println(s+": Roll No: "+r);
 		};
 		lam.m1("Sankha", 73);
+		lam.m2();
 		
 	}
 }
