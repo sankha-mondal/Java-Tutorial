9bc3dd0d7e9254f1aeac71e7ef8cc918add3bb87
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2022-03-14 16:51:51 +0530

    Supplier Functional Implementation

--- Lambda Expressions/Supplier_Implementation.java
+++ Lambda Expressions/Supplier_Implementation.java
@@ -1,5 +1,6 @@
 //  Supplier Functional Interface -> Don't take any input, but it returns output.
 //  It can be used all the context where there is no input but output expected.
+//  It has a functional method called T get() As Supplier is functional interface
 
 import java.util.function.Supplier;
 
