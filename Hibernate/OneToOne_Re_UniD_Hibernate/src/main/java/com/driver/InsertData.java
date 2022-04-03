1b16ab94085bc65ad03ee95ea2a28686bad6c9f3
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2022-04-03 21:28:30 +0530

    OneToOne relationShip Uni-directional

--- Hibernate/OneToOne_Re_UniD_Hibernate/src/main/java/com/driver/InsertData.java
+++ Hibernate/OneToOne_Re_UniD_Hibernate/src/main/java/com/driver/InsertData.java
@@ -12,29 +12,29 @@ public class InsertData {
 
 	public static void main(String[] args) {
 
-		SessionFactory factory = new Configuration()		// create session factory
-						.configure("hibernate.cfg.xml")
-						.addAnnotatedClass(Student.class)
-						.addAnnotatedClass(Address.class)
-						.buildSessionFactory();
+		SessionFactory factory = new Configuration()			// create session factory
+					.configure("hibernate.cfg.xml")
+					.addAnnotatedClass(Student.class)
+					.addAnnotatedClass(Address.class)
+					.buildSessionFactory();
 
-		Session session = factory.getCurrentSession();		// create session
+		Session session = factory.getCurrentSession();			// create session
 
 		try {
 
-			Student stuObj = new Student();		// create Student object
+			Student stuObj = new Student();				// create Student object
 			stuObj.setFirstName("Harish");
 			stuObj.setLastName("Sen");
 			stuObj.setEmail("sen.harish@hotmail.com");
 			
-			Address addObj = new Address();		// create Address object
+			Address addObj = new Address();				// create Address object
 			addObj.setState("Haryana");
 			addObj.setCity("Gurugram");
 			
-			stuObj.setStudentAddress(addObj);		// associate the objects
+			stuObj.setStudentAddress(addObj);			// associate the objects
 		
 
-			session.beginTransaction();			// start transaction
+			session.beginTransaction();		// start transaction
 
 				session.save(stuObj);	// save the Student
 				session.save(addObj);	// save the Student
@@ -42,6 +42,7 @@ public class InsertData {
 			session.getTransaction().commit();	// commit transaction
 			
 			System.out.println("Insert successfully...");
+			
 
 		} finally {
 			factory.close();
