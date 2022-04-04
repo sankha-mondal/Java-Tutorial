7a342bae002fcd3bfa7d5a2f0e12ec098152c058
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2022-04-04 12:21:52 +0530

    Hibernate Criteria Query > Retrieve data operation

--- Hibernate/Hibernate_Criteria_Query/src/main/java/com/DemoTest.java
+++ Hibernate/Hibernate_Criteria_Query/src/main/java/com/DemoTest.java
@@ -20,44 +20,39 @@ public class DemoTest {
 		SessionFactory sf = con.buildSessionFactory();
 		Session session = sf.openSession();
 		
-		System.out.println("Press 1.Retrieve all object:\nPress 2.Retrieve all objectwith condition:\n");
-		System.out.println("Press 3.Retrieve only one property i.e name:\nPress 4.Retrieve two property name,salary:\n");
-		System.out.println("Press 5.Aggregate function using Criteria:\n");
+		// Retrieve all object 
 		
-		// Retrieve all object [Press 1]
-		/**
-		Criteria cr	= session.createCriteria(Employee.class);
+		Criteria cr = session.createCriteria(Employee.class);
 		List<Employee> listOfEmp = cr.list();
 		System.out.println("Number of Employee "+listOfEmp.size());
 		listOfEmp.forEach(e->System.out.println(e));
-		*/
 		
-		// Retrieve all object with condition 	[Press 2]
-		/**
-		Criteria cr	= session.createCriteria(Employee.class);
 		
-		//Criterion cc = Restrictions.gt("salary", 15000f);
+		// Retrieve all object with condition 	
+		
+		Criteria cr = session.createCriteria(Employee.class);
+		Criterion cc = Restrictions.gt("salary", 15000f);
 		//Criterion cc = Restrictions.between("id", 102, 104);
 		//Criterion cc = Restrictions.and(Restrictions.like("name", "Ritu"),Restrictions.eq("id", 102));
-		Criterion cc = Restrictions.or(Restrictions.like("name", "Ritu"),Restrictions.eq("id", 104));
+		//Criterion cc = Restrictions.or(Restrictions.like("name", "Ritu"),Restrictions.eq("id", 104));
 		cr.add(cc);
 		List<Employee> listOfEmp = cr.list();
 		System.out.println("Number of Employee "+listOfEmp.size());
 		listOfEmp.forEach(e->System.out.println(e));
-		*/
 		
-		// Retrieve only one property i.e name 		[Press 3]
-		/**
-		Criteria cr	= session.createCriteria(Employee.class);
+		
+		// Retrieve only one property i.e name 		
+		
+		Criteria cr = session.createCriteria(Employee.class);
 		cr.setProjection(Projections.property("name"));
 		List<String> listOfEmp = cr.list();
 		System.out.println("Number of Employee "+listOfEmp.size());
 		listOfEmp.forEach(e->System.out.println(e));
-		*/
 		
-		// Retrieve two property name,salary 		[Press 4]
-		/**
-		Criteria cr	= session.createCriteria(Employee.class);
+		
+		// Retrieve two property name,salary 		
+		
+		Criteria cr = session.createCriteria(Employee.class);
 		Projection p1 = Projections.property("name");
 		Projection p2 = Projections.property("salary");
 		ProjectionList pp = Projections.projectionList();
@@ -67,18 +62,18 @@ public class DemoTest {
 		List<Object[]> listOfEmp = cr.list();
 		System.out.println("Number of Employee "+listOfEmp.size());
 		listOfEmp.forEach(e->System.out.println("Name "+e[0]+"| Salary "+e[1]));
-		*/
+		
 
-		// Aggregate function using Criteria		[Press 5]
-		/**
-		Criteria cr	= session.createCriteria(Employee.class);
-		//cr.setProjection(Projections.max("salary"));
+		// Aggregate function using Criteria		
+		
+		Criteria cr = session.createCriteria(Employee.class);
+		cr.setProjection(Projections.max("salary"));
 		//cr.setProjection(Projections.min("salary"));
-		cr.setProjection(Projections.sum("salary"));
+		//cr.setProjection(Projections.sum("salary"));
 		
 		List<Float> ll =cr.list();
 		System.out.println("salary "+ll.get(0));
-		*/
+		
 	}
 
 }
