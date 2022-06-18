739f49216b045d84201bd0bfe2ff19e777f99517
Sankha Subhra Mondal <sankha123@gmail.com>
2022-06-19 01:38:36 +0530

    Data Types - TypeScript Operation

---
+++ Web Development/TypeScript/dataTypes.ts
@@ -0,0 +1,24 @@
+let id1:number=100;
+//id1="Ravi";   Error ❌❌❌
+let name1:string ="Ravi Kumar";
+let res1:boolean = true;
+let msg1:any="Welcome";
+msg1=100;
+msg1=true;
+
+let id2=200;            // base upon the it consider that type of data types. 
+let name2="Ajay Kumar";
+let res2 = false;
+let msg2="Welcome";
+//id2="Ramesh"   Error ❌❌❌
+
+let id3:number;
+let name3:string;
+let res3:boolean;
+
+id3 = 300;
+name3 = "Mahesh";
+res3=false;
+console.log(id1);
+console.log(name3);
+console.log(res3);
