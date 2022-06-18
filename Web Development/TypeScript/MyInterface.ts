fea87cc39e0a06e03a5dfeed940680e6a92a4f35
Sankha Subhra Mondal <sankha123@gmail.com>
2022-06-19 01:41:08 +0530

    Interface - TypeScript Operation

---
+++ Web Development/TypeScript/MyInterface.ts
@@ -0,0 +1,8 @@
+interface MyInterface {
+    id:number;           // you can't initialized only decleare.
+    name:string;         // you can't initialized only decleare.
+
+    dis1():void;         // you can't initialized only decleare.
+    disInfo(): string;   // you can't initialized only decleare.
+    
+}
