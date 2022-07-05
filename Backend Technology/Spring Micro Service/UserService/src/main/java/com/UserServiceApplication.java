55124b8448274fe0fcd3a0f6f9a8626305917c51
Sankha Subhra Mondal <sankha123@gmail.com>
2022-07-05 17:07:50 +0530

    Spring Boot with Data-> [ CRUD | Rest client | swagger:8761 | Eureka ]

--- Backend Technology/Spring Micro Service/UserService/src/main/java/com/UserServiceApplication.java
+++ Backend Technology/Spring Micro Service/UserService/src/main/java/com/UserServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication			// Use to enable @Restcontroller, @Service and @Repository 
 @EnableEurekaClient
-@EntityScan(basePackages = "com.bean")
-@EnableJpaRepositories(basePackages = "com.repository")
+@EntityScan(basePackages = "com.bean")	// Use to enable entity class
+@EnableJpaRepositories(basePackages = "com.repository")		// Jpa Repository
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
