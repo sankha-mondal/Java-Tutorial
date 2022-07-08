0b7fd5ff82faad63e79b427f74000069796297ed
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2022-07-08 21:27:36 +0530

    OneToOne relationShip Uni-directional Type-2

--- Hibernate/OneToOne_Re_UniD_Hibernate/src/main/java/com/entity/Student.java
+++ Hibernate/OneToOne_Re_UniD_Hibernate/src/main/java/com/entity/Student.java
@@ -36,39 +36,30 @@ public class Student {
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getFirstName() {
 		return firstName;
 	}
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
 	public String getLastName() {
 		return lastName;
 	}
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public Address getStudentAddress() {
 		return studentAddress;
 	}
-
 	public void setStudentAddress(Address studentAddress) {
 		this.studentAddress = studentAddress;
 	}
