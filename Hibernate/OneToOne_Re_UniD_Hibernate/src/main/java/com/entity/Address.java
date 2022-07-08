2245453ea35bcddf18fa000b2d09786227cdcffe
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2022-07-08 21:28:19 +0530

    OneToOne RelationShip Uni-directional Type-2

--- Hibernate/OneToOne_Re_UniD_Hibernate/src/main/java/com/entity/Address.java
+++ Hibernate/OneToOne_Re_UniD_Hibernate/src/main/java/com/entity/Address.java
@@ -25,23 +25,18 @@ public class Address {
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getState() {
 		return state;
 	}
-
 	public void setState(String state) {
 		this.state = state;
 	}
-
 	public String getCity() {
 		return city;
 	}
-
 	public void setCity(String city) {
 		this.city = city;
 	}
