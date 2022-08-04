a50bf28a28726d03799c157cfd443d5a43f5676f
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2022-08-04 13:09:46 +0530

    OneToMany relationShip Bi-directional

--- Hibernate/OneToMany_Re_BiD_Hibernate/src/main/java/com/entity/Student.java
+++ Hibernate/OneToMany_Re_BiD_Hibernate/src/main/java/com/entity/Student.java
@@ -15,95 +15,32 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="student")
-public class Student {
-
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="id")
-	private int id;
+public class Project {
 	
-	@Column(name="first_name")
-	private String firstName;
-	
-	@Column(name="last_name")
-	private String lastName;
-	
-	@Column(name="email")
-	private String email;
-	
-	//Set up mapping between Student and Courses table
-	@OneToMany(mappedBy="student",
-			   cascade= {CascadeType.PERSIST, CascadeType.MERGE,CascadeType.DETACH, CascadeType.REFRESH})
-	private List<Course> courses;
-	
-	
-
-	@Override
-	public String toString() {
-		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
-				+  "]";
-	}
-
-	public Student() {
+		@Id
+		@Column(name = "product_id")
+		private int id;
 		
-	}
-
-	public Student(String firstName, String lastName, String email) {
-		super();
+		@Column(name = "product_name")
+		private String projectName;
 		
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public List<Course> getCourses() {
-		return courses;
-	}
-
-	public void setCourses(List<Course> courses) {
-		this.courses = courses;
-	}
-
-	//adding a convenience method for bi-directional relationship
-	public void add(Course tempCourse) {
-		if(courses==null) {
-			courses = new ArrayList<>();
-		}
-		courses.add(tempCourse);
-		tempCourse.setStudent(this);
-	}
+		@OneToMany(mappedBy = "project",
+				cascade= {CascadeType.PERSIST, CascadeType.MERGE,CascadeType.DETACH, CascadeType.REFRESH})
+	//	@JoinColumn(name="P_E_id", referencedColumnName = "product_id")  ❌❌❌
+		private List<Employees> listOfEmp;	// Project has many Employees
+		
+		/**
+		 *  1. The PK of Project table becomes the FK of Employees table.
+		 *  2. "P_E_id" column will be created in Employees table.
+		 *  
+		 *  3. referencedColumnName must not be in this class.
+		 *  
+		 *  
+		 *  4. [mappedBy] is used to give the preference of creating column
+		 *     in Employees table.
+		 *     
+		 *  5. [mappedBy] name should be as same as the reference of the
+		 *     Project of Employees class here mappedBy = "project".
+		 */
 		
 }
