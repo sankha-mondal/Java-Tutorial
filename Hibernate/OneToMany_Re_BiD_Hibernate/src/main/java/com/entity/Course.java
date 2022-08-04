2a696f700418b401d0bbe6da4d715350ab97fac0
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2022-08-04 13:10:27 +0530

    OneToMany relationShip Bi-directional

--- Hibernate/OneToMany_Re_BiD_Hibernate/src/main/java/com/entity/Course.java
+++ Hibernate/OneToMany_Re_BiD_Hibernate/src/main/java/com/entity/Course.java
@@ -11,63 +11,19 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="course")
-public class Course {
-
+public class Employees {
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="id")
+	@Column(name = "employee_id")
 	private int id;
 	
-	@Column(name="name")
+	@Column(name = "employee_name")
 	private String name;
 	
-	//We should not use CascadeType.Delete here
 	@ManyToOne(cascade= {CascadeType.PERSIST, CascadeType.MERGE,
-			             CascadeType.DETACH, CascadeType.REFRESH})
-	@JoinColumn(name="student_id")
-	private Student student;
-	
-	public Course() {
-		
-	}
-
-	public Course(String name) {
-		this.name = name;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	
-
-	public Student getStudent() {
-		return student;
-	}
-
-	public void setStudent(Student student) {
-		this.student = student;
-	}
-
-	@Override
-	public String toString() {
-		return "Course [id=" + id + ", name=" + name + "]";
-	}
-	
+                            CascadeType.DETACH, CascadeType.REFRESH})
+	@JoinColumn(name = "P_E_id")
+	private Project project;
 	
 	
 	
