be11f6b13275bd94dac5dde1bd77c53a35a5560b
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2022-08-04 12:02:31 +0530

    OneToOne RelationShip Bi-directional

--- Hibernate/OneToOne_Re_BiD_Hibernate/src/main/java/com/entity/Student.java
+++ Hibernate/OneToOne_Re_BiD_Hibernate/src/main/java/com/entity/Student.java
@@ -33,50 +33,14 @@ public class Student {
 	 *  1. (cascade = CascadeType.ALL) if we use this then accordingly 
 	 *     all the mapped date will be effected. Like on Delete operation 
 	 *     the data will delete from mapped table. 
+	 *
 	 *  2. The PK of Address table becomes FK i.e S_A_id of Student table.
+	 *
 	 *  3. "S_A_id" column will be created in Student table.
 	 *    
 	 */
 	
 	
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Address getStudentAddress() {
-		return studentAddress;
-	}
-
-	public void setStudentAddress(Address studentAddress) {
-		this.studentAddress = studentAddress;
-	}		
+		
 	
 }
