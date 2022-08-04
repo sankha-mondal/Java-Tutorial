e672ff825ae8c740d12cf69bfbe5ba159ea443e6
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2022-08-04 12:14:39 +0530

    OneToMany relationShip Uni-directional

--- Hibernate/OneToMany_Re_UniD_Hibernate/src/main/java/com/Employees.java
+++ Hibernate/OneToMany_Re_UniD_Hibernate/src/main/java/com/Employees.java
@@ -7,18 +7,25 @@ import javax.persistence.Id;
 public class Employees {
 	
 	@Id
+	@Column(name = "employee_id")
 	private int id;
+	
+	@Column(name = "employee_name")
 	private String name;
 	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
