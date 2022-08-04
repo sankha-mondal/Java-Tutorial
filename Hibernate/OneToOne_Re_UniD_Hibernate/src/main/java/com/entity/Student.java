a696601e2a44fac2f8c2a58d6c7b8d3886814bc4
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2022-08-04 11:30:51 +0530

    OneToOne RelationShip Uni-directional Type-2

--- Hibernate/OneToOne_Re_UniD_Hibernate/src/main/java/com/entity/Student.java
+++ Hibernate/OneToOne_Re_UniD_Hibernate/src/main/java/com/entity/Student.java
@@ -29,9 +29,18 @@ public class Student {
 	private String email;
 	
 	//Set up mapping between student & Address table | Student has Address
-	@OneToOne(cascade=CascadeType.ALL)
-	@JoinColumn(name="address_id")
-	private Address studentAddress;
+	@OneToOne //(cascade = CascadeType.ALL)      
+	@JoinColumn(name = "S_A_id")
+	private Address address;
+	
+	/**
+	 *  1. (cascade = CascadeType.ALL) if we use this then accordingly 
+	 *     all the mapped date will be effected. Like on Delete operation 
+	 *     the data will delete from mapped table. 
+	 *  2. The PK of Address table becomes FK i.e S_A_id of Student table.
+	 *  3. "S_A_id" column will be created in Student table.
+	 *    
+	 */
 
 	public int getId() {
 		return id;
