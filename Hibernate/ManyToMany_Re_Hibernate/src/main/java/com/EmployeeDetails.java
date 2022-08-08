6adec04e0fe7090bc7aedea9adbac8f5e8931714
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2022-08-08 13:31:32 +0530

    ManyToMany Relationship

--- Hibernate/ManyToMany_Re_Hibernate/src/main/java/com/EmployeeDetails.java
+++ Hibernate/ManyToMany_Re_Hibernate/src/main/java/com/EmployeeDetails.java
@@ -2,37 +2,56 @@ package com;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class EmployeeDetails {
+public class Employees {
+	
 	@Id
-	private int id;
-	private String name;
+	@Column(name = "e_id")
+	private int eId;
+	
+	@Column(name = "employee_name")
+	private String employeeName;
+	
 	@ManyToMany
-	private List<SkillSet> listOfSkill;
-	
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public List<SkillSet> getListOfSkill() {
-		return listOfSkill;
-	}
-	public void setListOfSkill(List<SkillSet> listOfSkill) {
-		this.listOfSkill = listOfSkill;
+	@JoinTable(
+				name = "new_p_e_join",
+				joinColumns = { @JoinColumn(name = "Employee_id")},
+				inverseJoinColumns = { @JoinColumn(name = "Project_id")}
+			  )
+	private List<Project> projects;
+
+	public int geteId() {
+		return eId;
 	}
 
+	public void seteId(int eId) {
+		this.eId = eId;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public List<Project> getProjects() {
+		return projects;
+	}
+
+	public void setProjects(List<Project> projects) {
+		this.projects = projects;
+	}
+		
+
 }
+
 
