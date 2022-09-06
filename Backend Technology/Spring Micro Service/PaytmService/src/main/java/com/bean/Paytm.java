886e5f49d2f0d0ac287854bad18906c97f1d34e1
Sankha Subhra Mondal <sankha123@gmail.com>
2022-09-06 20:18:05 +0530

    Paytm Service

---
+++ Backend Technology/Spring Micro Service/PaytmService/src/main/java/com/bean/Paytm.java
@@ -0,0 +1,25 @@
+package com.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Paytm {
+	
+	@Id
+	private int pid;
+	private Integer accno;
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public Integer getAccno() {
+		return accno;
+	}
+	public void setAccno(Integer accno) {
+		this.accno = accno;
+	}
+
+}
---
+++ Backend Technology/Spring Micro Service/PaytmService/src/main/java/com/controller/PaytmController.java
@@ -0,0 +1,25 @@
+package com.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.service.PaytmService;
+
+@RestController
+@RequestMapping("paytm")
+public class PaytmController {
+
+	
+	@Autowired
+	PaytmService paytmService;
+	
+	@GetMapping(value = "findAccount/{pid}")
+	public String findAccountDetails(@PathVariable("pid") int pid) {
+	
+		return paytmService.findAccounBalance(pid);
+		
+	}
+}
