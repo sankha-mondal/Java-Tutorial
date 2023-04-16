930410a5e905efd5163fb73237d490b0198a1a4c
Sankha Subhra Mondal <sankha123@gmail.com>
2023-04-16 19:51:20 +0530

    Set -> [HashSet] all Methods in one

--- Collection/Set_all_Methods.java
+++ Collection/Set_all_Methods.java
@@ -57,11 +57,23 @@ System.out.println("\n----------------------------------------------------------
 
 System.out.println("\n------------------------------------------------------------------");	
 
-		Iterator itr = car.descendingIterator();
-        		System.out.println("Elements in Reverse Order:");
+
+        		TreeSet<String> tree = new TreeSet<String>();
+		
+		System.out.println("HashSet add() method: To add list elements");
+		    tree.add("Mango");
+		    tree.add("Apple");
+		    tree.add("Onion");                        //  To add list elements  					  //  add()
+		    tree.add("Karrot");
+		    tree.add("Orange");
+		    tree.add("Leamon");
+		    System.out.println(tree);
+		    
+		   Iterator itr = car.descendingIterator();   //  Applicable for TreeSet Only
+        		   System.out.println("Elements in Reverse Order:");
         			while (itr.hasNext()) {
        				System.out.println(itr.next());
-        		}
+        		                 }
 
 
 
