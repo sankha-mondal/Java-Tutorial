4446ac85d48e31ed1b088693b79c807e87cd0e6c
Sankha Subhra Mondal <sankha123@gmail.com>
2023-07-24 01:17:02 +0530

    Example of Basic-Thread

---
+++ Thread/Basic_Thread_Exm.java
@@ -0,0 +1,26 @@
+public class Basic_Thread_Exm {
+	public static void main(String[] args) {
+	    
+	    Task taskRunner1 = new Task();
+		taskRunner1.start();
+		
+		// taskRunner1.start();  // we can't
+	    
+		System.out.println("Hello there..");
+		
+		Task taskRunner2 = new Task();
+		taskRunner2.start();
+		
+		
+	}
+}
+
+class Task extends Thread {
+    
+    public void run() {
+        for(int i=0;i<5;i++) {
+            System.out.println("Number "+i);
+        }
+    }
+    
+}
