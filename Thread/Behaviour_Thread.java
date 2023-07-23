14f1ff4b00895dcb2dd0b04f76c41946b48c0170
Sankha Subhra Mondal <sankha123@gmail.com>
2023-07-24 02:01:54 +0530

    Behaviour of Thread

--- Thread/Behaviour_Thread.java
+++ Thread/Behaviour_Thread.java
@@ -1,10 +1,13 @@
 
 
 
-class Thread1 extends Thread {
+class Thread1 extends Thread {   // Type: 1 - Using extends Thread
 	public void run() {
 		try {
-			System.out.println("Thread 1 is running");
+			for(int i=0;i<5;i++) {
+			    Thread.sleep(1000);
+			    System.out.println("Thread-1 is running");
+			}
 		}
 		catch (Exception e) {
 			System.out.println("Something went wrong: " + e);
@@ -13,10 +16,13 @@ class Thread1 extends Thread {
 }
 
 
-class Thread2 implements Runnable {
+class Thread2 implements Runnable {   // Type: 2 - Using implements Runnable
 	public void run() {
 		try {
-			System.out.println("Thread 2 is running");
+			for(int i=0;i<5;i++) {
+			    Thread.sleep(500);
+			    System.out.println("Thread-2 is running");
+			}
 		}
 		catch (Exception e) {
 			System.out.println("Something went wrong: " + e);
@@ -28,13 +34,28 @@ class Thread2 implements Runnable {
 public class Behaviour_Thread {
 	public static void main(String[] args) {
 
-		
-		for(int i=0;i<5;i++) {
-			Thread1 t1 = new Thread1();
+			Thread1 t1 = new Thread1();  // Type: 1 - Using extends Thread
 			t1.start();
 			
-			Thread t2 = new Thread(new Thread2());
+			Thread t2 = new Thread(new Thread2());   // Type: 2 - Using implements Runnable
 			t2.start();
-		}
+			
+			Thread t3 = new Thread(new Runnable() {   // Type: 3 - Using Anonymous class 
+			    
+			    @Override
+			    public void run() {
+			           try {
+			                for(int i=0;i<5;i++) {
+			                	      Thread.sleep(400);
+			                       System.out.println("Thread-3 is running");
+			                }
+			           }
+		            	           catch (Exception e) {
+			                   System.out.println("Something went wrong: " + e);
+		                            }
+			    }
+			});
+			t3.start();
+		
 	}
 }
