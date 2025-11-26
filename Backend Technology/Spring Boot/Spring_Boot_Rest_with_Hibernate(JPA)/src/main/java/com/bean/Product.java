531952f9b7fb7013394bd131f4b01eb9c5a74f49
sankha-mondal <sankha.github@gmail.com>
2025-11-26 21:54:53 +0530

    Spring Boot with Hibernate(JPA)

---
+++ Backend Technology/Spring Boot/Spring_Boot_Rest_with_Hibernate(JPA)/src/main/java/com/bean/Product.java
@@ -0,0 +1,53 @@
+package com.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+	@Id
+	private int pid;
+	private String pname;
+	private float price;
+	
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	
+	public Product(int pid, String pname, float price, String url) {
+		super();
+		this.pid = pid;
+		this.pname = pname;
+		this.price = price;
+	}
+	
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Product [pid=" + pid + ", pname=" + pname + ", price=" + price + "]";
+	}
+
+}
+
+
+
